eb8cb748bd5763c36b5622729e88431448d00ee4
MaxFusteCosta <116903233+MaxFusteCosta@users.noreply.github.com>
2022-11-18 13:32:33 +0100

    Update data_loader.py

--- Python 3/Double Pendulum/scinet/data_loader.py
+++ Python 3/Double Pendulum/scinet/data_loader.py
@@ -26,11 +26,13 @@ def load(validation_size_p, file_name):
     """
     f = gzip.open(io.data_path + file_name + ".plk.gz", 'rb')
     data, states = pickle.load(f)
+    data = np.array(data)
     states = np.array(states)
     train_val_separation = int(len(data[0]) * (1 - validation_size_p / 100.))
-    training_data = data[:train_val_separation]
-    training_states = states[:train_val_separation]
-    validation_data = data[train_val_separation:]
-    validation_states = states[train_val_separation:]
+    training_data = data[0][:train_val_separation]
+    training_states = states[0][:train_val_separation]
+    validation_data = data[0][train_val_separation:]
+    validation_states = states[0][train_val_separation:]
     f.close()
     return (training_data, validation_data, training_states, validation_states)
+
